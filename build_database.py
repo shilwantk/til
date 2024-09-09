1e66a50623caf33c2971589347f687d38b42af19
Kirti S <shilwantkirti@gmail.com>
2024-09-09 19:41:04 +0530

    Update build_database.py

--- build_database.py
+++ build_database.py
@@ -40,7 +40,7 @@ def build_database(repo_path):
         title = fp.readline().lstrip("#").strip()
         body = fp.read().strip()
         path = str(filepath.relative_to(root))
-        url = "https://github.com/shilwantk/til/blob/main".format(path)
+        url = "https://github.com/shilwantk/til/blob/main/".format(path)
         record = {
             "path": path.replace("/", "_"),
             "topic": path.split("/")[0],
